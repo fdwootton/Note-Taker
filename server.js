1515fada77b4df7a767b12438e7c3f87aa43a6f9
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-18 07:57:08 -0500

    Added js script to note.html

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Routes (GET)
+
+
+//API Routes
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db', 'db.json'))); //Show notes
+
+app.post('/api/notes', (req, res) => { //Save notes
+    let newNote = JSON.stringify(req.body);
+}); 
+
+//HTML Routes (GET)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'))); //Home page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html'))); //Note page
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
-
-//Routes (POST)
-app.post('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'))); //All other URLs go to Home Page
 
 // Listener for Server
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
